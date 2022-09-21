d4beeaa2cf641f623958e8ddb0c9f06b5745faa4
Keshava369 <101726271+Keshava369@users.noreply.github.com>
2022-09-21 09:21:01 +0530

    Update xy.py

--- xy.py
+++ xy.py
@@ -31,10 +31,14 @@ total_sales = int(df_select["Number_of_Sales"].sum())
 sales_usd = int(df_select["Sales_USD"].sum())
 
 
-st.subheader("Total Sales:")
-st.subheader(f" {total_sales:,}")
-st.subheader("Sales in USD")
-st.subheader(f"US $ {sales_usd:,}")
+left_co, right_co = st.columns(2)
+with left_co:
+    st.subheader("Total Sales:")
+    st.subheader(f" {total_sales:,}")
+with right_co:
+    st.subheader("Sales in USD")
+    st.subheader(f"US $ {sales_usd:,}")
+    
     
 st.markdown("---")
 
